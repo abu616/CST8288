95395924fa04016e8990edf6199a315edd9d768f
[Andrew Held] <held0010@algonquinlive.com>
2018-10-10 22:41:16 -0400

    Horizontal Line bug needs fixing

--- CST8288_MapMaker/src/mapMaker/MapMakerTools.java
+++ CST8288_MapMaker/src/mapMaker/MapMakerTools.java
@@ -14,18 +14,18 @@ public class MapMakerTools  extends ToolBar{
 	
 	public MapMakerTools() {
 		super.getItems().addAll(
-				getButton( "Select", (e) -> ToolState.getState().setTool(Tools.SELECTION)),
-				getButton( "Move", (e) -> ToolState.getState().setTool(Tools.MOVE)),
+				getButton( "Select", (e) -> ToolState.getState().setTool( Tools.SELECTION)),
+				getButton( "Move", (e) -> ToolState.getState().setTool( Tools.MOVE)),
 				getMenuButton( "Room", (e) -> {}),
-				getButton( "Path", (e) -> ToolState.getState().setTool(Tools.PATH)),
-				getButton( "Erase", (e) -> ToolState.getState().setTool(Tools.ERASE)),
-				getButton( "Door", (e) -> ToolState.getState().setTool(Tools.DOOR)));
+				getButton( "Path", (e) -> ToolState.getState().setTool( Tools.PATH)),
+				getButton( "Erase", (e) -> ToolState.getState().setTool( Tools.ERASE)),
+				getButton( "Door", (e) -> ToolState.getState().setTool( Tools.DOOR)));
 	}
 	
 	public Button getButton( String name, EventHandler<ActionEvent> handler) {
 		Label icon = new Label();
 		icon.setId( name + "-icon");
-		Button button = new Button(name, icon);
+		Button button = new Button( name, icon);
 		button.setId( name);
 		button.setText( "");
 		button.setOnAction( handler);
@@ -40,23 +40,23 @@ public class MapMakerTools  extends ToolBar{
 		menuButton.setOnAction( handler);
 		menuButton.getItems().addAll(			
 			createMenuItem( "Line", (e) -> {
-				ToolState.getState().setTool(Tools.ROOM);
+				ToolState.getState().setTool( Tools.ROOM);
 				ToolState.getState().setOption(2);
 			}),
 			createMenuItem( "Triangle", (e) -> {
-				ToolState.getState().setTool(Tools.ROOM);
+				ToolState.getState().setTool( Tools.ROOM);
 				ToolState.getState().setOption(3);
 			}),
 			createMenuItem( "Rectangle", (e) -> {
-				ToolState.getState().setTool(Tools.ROOM);
+				ToolState.getState().setTool( Tools.ROOM);
 				ToolState.getState().setOption(4);
 			}),
 			createMenuItem( "Pentagon", (e) -> {
-				ToolState.getState().setTool(Tools.ROOM);
+				ToolState.getState().setTool( Tools.ROOM);
 				ToolState.getState().setOption(5);
 			}),
 			createMenuItem( "Hexagon", (e) -> {
-				ToolState.getState().setTool(Tools.ROOM);
+				ToolState.getState().setTool( Tools.ROOM);
 				ToolState.getState().setOption(6);
 			}));
 		return menuButton;
